5e34ac9ca84b7996d12b08d2689e4fd3ade67446
Jacobus Smit <jacobusmmsmit@gmail.com>
2022-08-09 18:07:20 +0100

    Add reverse_dims keyword for MultiscaleMixerBlock

--- multiscalemixer.py
+++ multiscalemixer.py
@@ -10,13 +10,17 @@ class MultiscaleMixerBlock(eqx.Module):
     mixers: list
     norms: list
 
-    def __init__(self, dim_sizes, width_sizes, *, key):
+    def __init__(self, dim_sizes, width_sizes, *, key, reverse_dims=True):
         keys = jr.split(key, len(dim_sizes))
+        if reverse_dims:
+            _dim_sizes = list(reversed(dim_sizes))
+        else:
+            _dim_sizes = dim_sizes
         self.mixers = [
             eqx.nn.MLP(dim_size, dim_size, width_size, depth=1, key=key)
             for dim_size, width_size, key in zip(dim_sizes, width_sizes, keys)
         ]
-        self.norms = [eqx.nn.LayerNorm(list(reversed(dim_sizes))) for _ in dim_sizes]
+        self.norms = [eqx.nn.LayerNorm(_dim_sizes) for _ in dim_sizes]
 
     def __call__(self, y):
         N = len(self.mixers)
