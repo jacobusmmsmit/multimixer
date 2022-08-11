fd78e302b994a725df936ff0f4adfe24485c61c0
Jacobus Smit <jacobusmmsmit@gmail.com>
2022-08-11 18:24:14 +0100

    Add helper functions for MultiMixer

---
+++ helpers.py
@@ -0,0 +1,36 @@
+import jax
+import einops
+
+import jax.numpy as jnp
+
+from jax import lax
+
+
+def divscan(init, array, length=None, reverse=False, unroll=1):
+    """
+    Return a `floored` division scan on array, beginning with init.
+    """
+
+    def floored_div(result, element):
+        result = jnp.floor_divide(result, element)
+        return result, result
+
+    return lax.scan(floored_div, init, array, length, reverse, unroll)[1]
+
+
+def arrange_patches(img, num_heights, num_widths):
+    """
+    equivalent to einops.rearrange(img, 'c (h a?) (w b?) -> (h w) (?a ?b) ... (c a b)', a=x, b=y)
+    """
+    res = einops.rearrange(
+        img,
+        "c (h nh) (w nw) -> h w (c nh nw)",
+        nh=int(num_widths[0]),
+        nw=int(num_heights[0]),
+    )
+    for nw, nh in zip(num_heights[1:], num_widths[1:]):
+        res = einops.rearrange(
+            res, "(h nh) (w nw) ... -> h w (nh nw) ...", nh=int(nh), nw=int(nw)
+        )
+    res = einops.rearrange(res, "h w ... -> (h w) ...")
+    return res
