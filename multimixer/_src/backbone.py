f19be990f3cf7901ba6d31dd6a87f1a8c45a520b
Jacobus Smit <jacobusmmsmit@gmail.com>
2022-08-31 15:03:33 +0100

    Implement choosing dims per block

--- multimixer/_src/backbone.py
+++ multimixer/_src/backbone.py
@@ -7,34 +7,49 @@ from .utils import antivmap
 
 
 class MultiMixerBlock(eqx.Module):
-    """Maps a different MLP over each dimension of the input from first to last."""
+    """Maps a different MLP over dims of the input specified by apply_dims."""
 
     mixers: List[eqx.nn.MLP]
     norms: List[eqx.nn.LayerNorm]
+    apply_dims: List[int]
 
-    def __init__(self, dimensions, mlp_widths, *, key):
+    def __init__(self, mixer_dimensions, mlp_widths, *, apply_dims=None, key):
         """**Arguments:**
-        - `dimensions`: The dimensions of the input and output.
+        - `mixer_dimensions`: The mixer_dimensions of the input and output.
         - `mlp_widths`: The width of the single hidden layer in the MLP of each dimension.
+
+        **Kwargs**
+        - `apply_dims`: Which dimensions to apply an MLP down, defaults to all
         - `key`: A `jax.random.PRNGKey` used to provide randomness for parameter
             initialisation. (Keyword only argument.)
         """
-        # dimensions are put in from local to global
-        assert len(dimensions) == len(mlp_widths)
-        mlp_keys = jr.split(key, len(dimensions))
-        self.mixers = [
-            eqx.nn.MLP(dim, dim, mlp_width, depth=1, key=mlp_key)
-            for dim, mlp_width, mlp_key in zip(dimensions, mlp_widths, mlp_keys)
-        ]
-        self.norms = [eqx.nn.LayerNorm(dimensions) for _ in dimensions]
+        # mixer_dimensions are put in from local to global
+        assert len(mixer_dimensions) == len(mlp_widths)
+        mlp_keys = jr.split(key, len(mixer_dimensions))
+
+        mixers = []
+        norms = []
+        for input_dim, (mixer_dim, mlp_width, mlp_key) in enumerate(
+            zip(mixer_dimensions, mlp_widths, mlp_keys)
+        ):
+            if input_dim in apply_dims:
+                mixers.append(
+                    eqx.nn.MLP(mixer_dim, mixer_dim, mlp_width, depth=1, key=mlp_key)
+                )
+                norms.append(eqx.nn.LayerNorm(mixer_dimensions))
+
+        self.mixers = mixers
+        self.norms = norms
+        self.apply_dims = apply_dims  # Example: [0, 1]
 
     def __call__(self, y):
         """**Arguments**
-        - `y`: The input. Should be of shape `(dimensions)`.
+        - `y`: The input. Should be of shape `(mixer_dimensions)`.
         """
         # TODO: improve compilation time by structured control flow primitives
         # something like: lax.fori_loop(0, len(self.mixers), vmap(mixer[i], i, i)(norm[i]), y)
-        for i, (mixer, norm) in enumerate(zip(self.mixers, self.norms)):
+
+        for i, mixer, norm in zip(self.apply_dims, self.mixers, self.norms):
             y = y + antivmap(mixer, i)(norm(y))
         return y
 
@@ -42,8 +57,6 @@ class MultiMixerBlock(eqx.Module):
 class MultiMixer(eqx.Module):
     """An MLP-Mixer with multiple patch dimensions/scales"""
 
-    input_shape: List[int]
-    mixer_dimensions: List[int]
     blocks: List[MultiMixerBlock]
     norm: eqx.nn.LayerNorm
 
@@ -67,13 +80,16 @@ class MultiMixer(eqx.Module):
         - `key`: A JAX PRNG key.
         """
         bkeys = jr.split(key, num_blocks)
+        if dims_per_block is None:
+            dims_per_block = [
+                list(range(len(mixer_dimensions))) for _ in range(num_blocks)
+            ]
 
-        self.mixer_dimensions = mixer_dimensions
         self.blocks = [
             MultiMixerBlock(
                 mixer_dimensions,
                 mixer_widths,
-                apply_dims,
+                apply_dims=apply_dims,
                 key=bkey,
             )
             for apply_dims, bkey in zip(dims_per_block, bkeys)
